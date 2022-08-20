97a3a2b6ebd1429678eeca0b366e0cdf1a934aa2
shineunjeong <dkfah1229@naver.com>
2022-08-20 23:51:55 +0900

    20220820

---
+++ Algorithm/src/BaekJoon/etc/P11724.java
@@ -0,0 +1,7 @@
+package BaekJoon.etc;
+
+public class P11724 {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Algorithm/src/BaekJoon/level3/P10818.java
@@ -0,0 +1,50 @@
+package BaekJoon.level3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 최소, 최대
+
+/* 문제
+ * N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+ */
+
+/* 입력
+ * 첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+5
+20 10 35 30 7
+ */
+
+/* 출력
+ * 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+7 35
+ */
+
+public class P10818 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			
+			if(min > num) {
+				min = num;
+			}
+			
+			if(max < num) {
+				max = num;
+			}
+		}
+		
+		sb.append(min).append(" ").append(max);
+		
+		System.out.println(sb);
+	}
+}
