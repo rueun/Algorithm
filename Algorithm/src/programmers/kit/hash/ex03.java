8c359308a3997c26a7845e7db2a1fc4d0fe82430
shineunjeong <dkfah1229@naver.com>
2022-08-25 23:07:47 +0900

    20220825

---
+++ Algorithm/src/programmers/kit/hash/ex03.java
@@ -0,0 +1,23 @@
+package programmers.kit.hash;
+
+import java.util.Arrays;
+
+public class ex03 {
+	
+	public static void main(String[] args) {
+		
+	}	
+	public boolean solution(String[] phone_book) {
+		Arrays.sort(phone_book);
+		
+		for(int i=0; i<phone_book.length-1; i++) {
+			if(phone_book[i+1].startsWith(phone_book[i])) {
+				return false;
+			}
+		}
+		
+		return true;
+    }
+	
+	
+}
